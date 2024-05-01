b3a4bde1d9c7b490416aa9a9f205404a2a6becd0
ruiqurm <ruiqurm@gmail.com>
2024-05-01 21:18:45 +0800

    debug physical address resolving

--- src/device/hardware/phys_addr_resolver.rs
+++ src/device/hardware/phys_addr_resolver.rs
@@ -1,5 +1,6 @@
-use std::{fs::File, io, mem::size_of, os::fd::AsRawFd, process};
+use std::{fs::File, io, mem::size_of, os::fd::AsRawFd, process, ptr::addr_of_mut};
 
+use log::error;
 
 const PAGE_SHIFT: u64 = 12; // Typical page size shift for 4KB pages
 const PAGE_SIZE: usize = 1 << PAGE_SHIFT;
@@ -12,39 +13,47 @@ pub(crate) struct PhysAddrResolver {
 
 impl PhysAddrResolver {
     pub(crate) fn new() -> io::Result<PhysAddrResolver> {
-        let pagemap_file = format!("/proc/{}/pagemap",process::id());
+        let pagemap_file = format!("/proc/{}/pagemap", process::id());
         let pagemap = File::open(pagemap_file)?;
         let pagemap_fd = pagemap.as_raw_fd();
         Ok(PhysAddrResolver {
-            _pagemap : pagemap,
+            _pagemap: pagemap,
             pagemap_fd,
         })
     }
 
-    pub(crate) fn query(&self,vaddr: usize) -> Option<usize>{
+    #[allow(
+        clippy::borrow_as_ptr,
+        clippy::arithmetic_side_effects,
+        clippy::cast_possible_wrap,
+        clippy::cast_sign_loss
+    )]
+    pub(crate) fn query(&self, vaddr: usize) -> Option<usize> {
         let vpn = vaddr >> PAGE_SHIFT;
         let offset = vaddr % PAGE_SIZE;
-        let mut nread = 0;
         let pagemap_fd = self.pagemap_fd.as_raw_fd();
         let data: u64 = 0;
-        #[allow(clippy::borrow_as_ptr,clippy::arithmetic_side_effects,clippy::cast_possible_wrap,clippy::cast_sign_loss)]
-        while nread < size_of::<u64>() {
-            let ret = unsafe {
-                libc::pread(
-                    pagemap_fd,
-                    &data as *const _ as *mut libc::c_void,
-                    size_of::<u64>() - nread,
-                    (vpn * size_of::<u64>() + nread) as libc::off_t,
-                )
-            };
-            nread = nread.wrapping_add(ret as usize);
-            if ret <= 0 {
-                return None;
-            }
+        error!("query {:x}",vaddr);
+        let ret = unsafe{libc::lseek(pagemap_fd, (vpn * 8) as i64, libc::SEEK_SET)};
+        if ret < 0{
+            error!("lseek failed :{:?}",io::Error::last_os_error());
+        }
+        if unsafe {
+            libc::read(
+                pagemap_fd,
+                &data as *const _ as *mut libc::c_void,
+                size_of::<u64>(),
+            )
+        } != 8
+        {
+            error!("read failed");
+            return None;
         }
         let pfn = data & ((1 << 55_i32) - 1);
-        #[allow(clippy::arithmetic_side_effects,clippy::cast_possible_truncation)] // copy from C code
+        #[allow(clippy::arithmetic_side_effects, clippy::cast_possible_truncation)]
+        // copy from C code
         let phsy_addr = (pfn << PAGE_SHIFT | (offset as u64)) as usize;
+        error!("phsy_addr: {phsy_addr}");
         Some(phsy_addr)
     }
 }
