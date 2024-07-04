444c1f554eb34c178f9dd58f4b6013c603aa45e8
ruiqurm <ruiqurm@gmail.com>
2024-07-04 09:14:48 +0000

    initialize rust-driver

---
+++ rust-driver/src/lib.rs
@@ -0,0 +1,140 @@
+//! open-rdma-driver
+#![deny(
+    // The following are allowed by default lints according to
+    // https://doc.rust-lang.org/rustc/lints/listing/allowed-by-default.html
+    absolute_paths_not_starting_with_crate,
+    explicit_outlives_requirements,
+    // elided_lifetimes_in_paths,  // allow anonymous lifetime
+    explicit_outlives_requirements,
+    keyword_idents,
+    macro_use_extern_crate,
+    meta_variable_misuse,
+    missing_abi,
+    missing_copy_implementations,
+    missing_debug_implementations, 
+    // must_not_suspend, unstable
+    missing_docs,
+    non_ascii_idents,
+    // non_exhaustive_omitted_patterns, unstable
+    noop_method_call,
+    pointer_structural_match,
+    rust_2021_incompatible_closure_captures,
+    rust_2021_incompatible_or_patterns,
+    rust_2021_prefixes_incompatible_syntax,
+    rust_2021_prelude_collisions,
+    single_use_lifetimes,
+    // trivial_casts, // We allow trivial_casts for casting a pointer
+    trivial_numeric_casts,
+    unreachable_pub,
+    unsafe_code,
+    unsafe_op_in_unsafe_fn,
+    unstable_features,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_lifetimes,
+    unused_qualifications,
+    unused_results,
+    variant_size_differences, 
+
+    clippy::all,
+    clippy::pedantic,
+    clippy::cargo,
+
+    // The followings are selected restriction lints for rust 1.57
+    // clippy::as_conversions, // we allow lossless "as" conversion, it has checked by clippy::cast_possible_truncation
+    clippy::clone_on_ref_ptr,
+    clippy::create_dir,
+    clippy::dbg_macro,
+    clippy::decimal_literal_representation,
+    clippy::default_numeric_fallback,
+    clippy::disallowed_script_idents,
+    clippy::else_if_without_else,
+    clippy::exhaustive_enums,
+    clippy::exhaustive_structs,
+    clippy::exit,
+    clippy::expect_used,
+    clippy::filetype_is_file,
+    clippy::float_arithmetic,
+    clippy::float_cmp_const,
+    clippy::get_unwrap,
+    clippy::if_then_some_else_none,
+    // clippy::implicit_return, it's idiomatic Rust code.
+    clippy::indexing_slicing,
+    clippy::inline_asm_x86_intel_syntax,
+    clippy::arithmetic_side_effects,
+ 
+    // clippy::pattern_type_mismatch, // cause some false postive and unneeded copy
+    clippy::print_stderr,
+    clippy::print_stdout,
+    clippy::rc_buffer,
+    clippy::rc_mutex,
+    clippy::rest_pat_in_fully_bound_structs,
+    clippy::same_name_method,
+    clippy::self_named_module_files,
+    // clippy::shadow_reuse, it’s a common pattern in Rust code
+    // clippy::shadow_same, it’s a common pattern in Rust code
+    clippy::shadow_unrelated,
+    clippy::str_to_string,
+    clippy::string_add,
+    clippy::string_to_string,
+    clippy::unimplemented,
+    clippy::unnecessary_self_imports,
+    clippy::unneeded_field_pattern,
+    // clippy::unreachable, // the unreachable code should unreachable otherwise it's a bug
+    clippy::unwrap_in_result,
+    clippy::unwrap_used, 
+    clippy::use_debug,
+    clippy::verbose_file_reads,
+    clippy::wildcard_enum_match_arm,
+
+    // // The followings are selected lints from 1.61.0 to 1.67.1
+    clippy::as_ptr_cast_mut,
+    clippy::derive_partial_eq_without_eq,
+    clippy::empty_drop,
+    clippy::empty_structs_with_brackets,
+    clippy::format_push_string,
+    clippy::iter_on_empty_collections,
+    clippy::iter_on_single_items,
+    clippy::large_include_file,
+    clippy::manual_clamp,
+    clippy::suspicious_xor_used_as_pow,
+    clippy::unnecessary_safety_comment,
+    clippy::unnecessary_safety_doc,
+    clippy::unused_peekable,
+    clippy::unused_rounding,    
+
+    // The followings are selected restriction lints from rust 1.68.0 to 1.71.0
+    // clippy::allow_attributes, still unstable
+    clippy::impl_trait_in_params,
+    clippy::let_underscore_untyped,
+    clippy::missing_assert_message,
+    clippy::multiple_unsafe_ops_per_block,
+    clippy::semicolon_inside_block,
+    // clippy::semicolon_outside_block, already used `semicolon_inside_block`
+    clippy::tests_outside_test_module,
+    // 1.71.0
+    clippy::default_constructed_unit_structs,
+    clippy::items_after_test_module,
+    clippy::manual_next_back,
+    clippy::manual_while_let_some,
+    clippy::needless_bool_assign,
+    clippy::non_minimal_cfg,
+)]
+
+#![cfg_attr(
+    test,
+    allow(
+        clippy::indexing_slicing,
+        unused_results,
+        clippy::unwrap_used,
+        clippy::unwrap_in_result,
+        clippy::expect_used,
+        clippy::as_conversions,
+        clippy::shadow_unrelated,
+        clippy::arithmetic_side_effects,
+        clippy::let_underscore_untyped,
+        clippy::pedantic, 
+        clippy::default_numeric_fallback,
+        clippy::print_stderr,
+    )
+)]
